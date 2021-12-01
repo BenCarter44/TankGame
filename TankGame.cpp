0eff42109be66bf1138d69d400b5613579b6ce32
BenRobotics101 <foothillben@gmail.com>
2021-12-01 14:38:15 -0700

    First Commit

---
+++ TankGame.cpp
@@ -0,0 +1,33 @@
+﻿// TankGame.cpp : Defines the entry point for the application.
+//
+
+
+/*
+Contain a store
+Contain ability to save player progress. (store on server, or load from file) 
+
+Player data:
+   - strength of tank
+   - bought items
+   - money
+   
+
+Follow the progression of ideas in a storyboard. Storyboard the game
+
+Come up with the requirements of the game. Some screenshots
+
+Make the game easy/simple. Then add things to it. Your storyboard will be your "contract". Your game will be graded against your storyboard
+
+
+
+
+*/
+#include "TankGame.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
